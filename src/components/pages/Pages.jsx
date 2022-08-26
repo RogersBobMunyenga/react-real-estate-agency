5a0ec7a7aa37f8056291ec53e30ab3dd77d3a438
RogersBobMunyenga <b.munyenga@gmail.com>
2022-08-26 15:15:47 +0300

    footer

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Header from '../common/header/Header';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from '../home/Home';
+import Footer from '../common/footer/Footer';
 const Pages = () => {
     return (
         <>
             <Router>
             <Header />
-
         <Switch>
           <Route exact path="/" component={Home} />
           
-        </Switch>
+          </Switch>
+            <Footer />     
     </Router>
         </>
   );
