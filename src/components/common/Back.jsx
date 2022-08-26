eff9fbf034a3bc184131bec564ce6c9424388063
RogersBobMunyenga <b.munyenga@gmail.com>
2022-08-27 00:45:10 +0300

    pricing and contact pages

--- src/components/common/Back.jsx
+++ src/components/common/Back.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react"
 
-const Back = (name, title, cover) => {
+const Back = ({ name, title, cover }) => {
   return (
-      <>
-          <div className="back">
-              <div className="container">
-                  <span>{name}</span>
-                  <h1>{ title}</h1>
-              </div>
-              <img src={cover} alt="" />
+    <>
+      <div className='back'>
+        <div className='container'>
+          <span>{name}</span>
+          <h1>{title}</h1>
+        </div>
+        <img src={cover} alt='' />
       </div>
-      </>
+    </>
   )
 }
 
