607875c6aee37dea000448a69054d12b52c82566
RogersBobMunyenga <b.munyenga@gmail.com>
2022-08-26 23:51:22 +0300

    services page

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -4,14 +4,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from '../home/Home';
 import Footer from '../common/footer/Footer';
 import About from '../about/About';
+import Services from '../services/Services';
 const Pages = () => {
     return (
         <>
             <Router>
             <Header />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/services' component={Services} />
           
           </Switch>
             <Footer />     
